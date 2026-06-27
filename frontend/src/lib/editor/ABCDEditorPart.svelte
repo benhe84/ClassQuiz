ba8f28da9ba24e398cde594f583bd2eecd9d8351
benhe84 <benhe84@web.de>
2026-06-27 11:57:08 +0200

    Update ABCDEditorPart.svelte

--- frontend/src/lib/editor/ABCDEditorPart.svelte
+++ frontend/src/lib/editor/ABCDEditorPart.svelte
@@ -17,7 +17,7 @@ SPDX-License-Identifier: MPL-2.0
 
 	const { t } = getLocalization();
 
-	const default_colors = ['#D6EDC9', '#B07156', '#7F7057', '#4E6E58'];
+	const default_colors = ['#6366F1', '#EC4899', '#F59E0B', '#10B981'];
 
 	interface Props {
 		selected_question: number;
