dcccb16f1f28a1024db0e3da046f75cfa5577136
bene84 <benhe84@web.de>
2026-06-28 10:23:20 +0200

    Neues Quizz

--- frontend/src/routes/create/+page.svelte
+++ frontend/src/routes/create/+page.svelte
@@ -77,7 +77,11 @@ SPDX-License-Identifier: MPL-2.0
 			>&#8203;</span
 		>
 		<div
-			class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+			class="inline-block align-bottom
+		       bg-white dark:bg-gray-800
+	       rounded-lg text-left overflow-hidden shadow-xl
+	       transform transition-all
+	       sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
 		>
 			<div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
 				<div class="sm:flex sm:items-start">
